63ca7261adec1b2644e0232644f8d4a8be36bab8
Ali Akbar <sayaliakbar@gmail.com>
2025-03-01 01:27:28 +0500

    add addTodo controller and update todos route for POST requests

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,10 +1,10 @@
 const Todo = require("../models/Todo");
 
 // @desc Get all todos
-// @route GET /api/v1/todos
+// @route GET /api/todos
 // @access Public
-exports.getTodos = async (req, res, next) => {
-    console.log("getTodos");
+
+exports.getTodos = async (req, res) => {
   try {
     const todos = await Todo.find();
 
@@ -20,3 +20,34 @@ exports.getTodos = async (req, res, next) => {
     });
   }
 };
+
+// @desc Add a todo
+// @route POST /api/todos
+// @access Public
+
+exports.addTodo = async (req, res)=>{
+    console.log(req.body);
+    try {
+        const { title } = req.body;
+    
+        if (!title) {
+        return res.status(400).json({
+            success: false,
+            error: "Please provide a todo item",
+        });
+        }
+    
+        const todo = await Todo.create(req.body);
+    
+        return res.status(201).json({
+        success: true,
+        data: todo,
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+        success: false,
+        error: "Server Error",
+        });
+    }
+}
