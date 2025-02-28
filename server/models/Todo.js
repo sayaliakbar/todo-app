211fe2e958224504d3522e56fd951d3051e38d0b
Ali Akbar <sayaliakbar@gmail.com>
2025-03-01 00:59:39 +0500

    enhance Todo model validation for title field with custom message and trim option

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const TodoSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
+        required: [true, "Please provide a title"],
+        trim: true,
     },
     completed: {
         type: Boolean,
