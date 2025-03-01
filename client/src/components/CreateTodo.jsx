3a73f520eb74b33ebff28b3083b47d0f5941506b
Ali Akbar <sayaliakbar@gmail.com>
2025-03-02 01:57:30 +0500

    add: implement todo input handling and submission in CreateTodo component

--- client/src/components/CreateTodo.jsx
+++ client/src/components/CreateTodo.jsx
@@ -1,11 +1,29 @@
-
+import { useAddTodoMutation } from '../services/todo.js'
+import {useState} from "react";
 
 
 export default function CreateTodo() {
+  const [todo,setTodo]=useState('');
+
+  const [addTodo] = useAddTodoMutation();
+  
 
 
-  const handleSubmit = (e) => {
+  const handleSubmit =async (e) => {
     e.preventDefault();
+   
+ 
+  
+  try {
+    await addTodo(todo);
+    
+   
+  } catch (error) {
+    console.log(error);
+  }finally{
+    setTodo('');
+    
+  }
     
   };
   return (
@@ -14,10 +32,11 @@ export default function CreateTodo() {
       className="w-full relative mt-4 sm:mt-0 xs:drop-shadow-lg"
     >
       <input
-   
         className="leading-none w-full py-4 sm:py-5 px-11 sm:px-16 rounded-lg text-xs sm:text-body dark:bg-gray-800 dark:hover:bg-gray-900 hover:bg-gray-200 cursor-pointer"
         type="text"
         placeholder="Create a new todo..."
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <div className="w-4 h-4 sm:w-6 sm:h-6 rounded-full border border-black/25 absolute top-3 sm:top-4 left-4 sm:left-5 dark:border-white/25"></div>
     </form>
