b2bfb29b04792797548ce16f363dcf84d6a0a84e
Ali Akbar <sayaliakbar@gmail.com>
2025-03-02 04:05:53 +0500

    add: enhance TodoListFooter to display dynamic item count and filtering options

--- client/src/components/TodoListFooter.jsx
+++ client/src/components/TodoListFooter.jsx
@@ -1,10 +1,48 @@
-import ShowTodoTaskButtons from "./ShowTodoTaskButtons";
-export default function TodoListFooter() {
+import { useGetTodosQuery } from "../services/todo";
+
+import PropTypes from "prop-types";
+
+export default function TodoListFooter({ filter, setFilter }) {
+  TodoListFooter.propTypes = {
+    filter: PropTypes.string.isRequired,
+    setFilter: PropTypes.func.isRequired,
+  };
+
+  const { data } = useGetTodosQuery();
+  const todos = data?.todoList;
+  const activeTodos = todos?.filter((todo) => !todo.completed);
+  const itemsLeft = activeTodos?.length;
+
   return (
     <div className=" flex justify-between text-xs sm:text-sm p-4 text-black/50 dark:text-white/50">
-      <p>5 items left</p>
+      <p>{itemsLeft} items left</p>
       <div className="hidden sm:block">
-        <ShowTodoTaskButtons></ShowTodoTaskButtons>
+        <div className="flex flex-row gap-4 font-bold text-black/50">
+          <button
+            onClick={() => setFilter("all")}
+            className={`${
+              filter === "all" ? "text-primary-brightBlue" : ""
+            } hover:text-black dark:hover:text-white`}
+          >
+            All
+          </button>
+          <button
+            onClick={() => setFilter("active")}
+            className={`${
+              filter === "active" ? "text-primary-brightBlue" : ""
+            } hover:text-black dark:hover:text-white`}
+          >
+            Active
+          </button>
+          <button
+            onClick={() => setFilter("completed")}
+            className={`${
+              filter === "completed" ? "text-primary-brightBlue" : ""
+            } hover:text-black dark:hover:text-white`}
+          >
+            Completed
+          </button>
+        </div>
       </div>
 
       <button className="hover:text-black dark:hover:text-white">
