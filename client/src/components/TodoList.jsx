6e4e40b8dca38f4c3f16847726676d2355f1ac65
Ali Akbar <sayaliakbar@gmail.com>
2025-03-02 00:56:47 +0500

    refactor TodoList component to use Redux Toolkit for data fetching and improve error/loading handling

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,39 +1,33 @@
 import TodoListFooter from "./TodoListFooter";
 import TodoListTask from "./TodoListTask";
-import { useEffect, useState } from "react";
-import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL;
+import { useGetTodosQuery } from '../services/todo.js'
 
 
 export default function TodoList() {
-  
-  const [todoList, setTodoList] = useState([]);
-
-  const fetchAPI = async () => {
-    const response = await axios.get(`${apiUrl}/api/todos`);
-    setTodoList(response.data.todoList);
-    console.log(response.data.todoList);
-  };
-  useEffect(() => {
-    fetchAPI();
-  }, [setTodoList]);
-
-  
-
+  const { data, error, isLoading } = useGetTodosQuery();
 
   return (
     <div className="w-full bg-white shadow-2xl rounded-lg overflow-hidden dark:bg-gray-800">
       <ul className="overflow-y-auto max-h-64 w-full  flex flex-col">
-        {todoList.length > 0 &&
-          todoList.map((todo) => (
-            <TodoListTask
-              key={todo._id}
-              id={todo._id}
-              text={todo.title}
-              status={todo.completed}
-            ></TodoListTask>
-          ))}
+        {error?(
+           <>Oh no, there was an error</>
+        ):
+        isLoading?(
+          <>Loading...</>
+        ):
+        data?(
+            data?.todoList.map((todo) => (
+              <TodoListTask
+                key={todo._id}
+                id={todo._id}
+                text={todo.title}
+                status={todo.completed}
+              ></TodoListTask>
+            ))
+        ):(
+          <>No data</>
+        )}
       </ul>
       <TodoListFooter />
     </div>
