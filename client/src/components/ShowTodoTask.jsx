9c9cb622f5c8d8efc02f02071a500b231e3d73d9
Ali Akbar <sayaliakbar@gmail.com>
2025-03-02 04:03:56 +0500

    remove: delete ShowTodoTask and ShowTodoTaskButtons components

--- client/src/components/ShowTodoTask.jsx
+++
@@ -1,8 +0,0 @@
-import ShowTodoTaskButtons from "./ShowTodoTaskButtons";
-export default function ShowTodoTask() {
-  return (
-    <div className="flex sm:hidden gap-4 font-bold text-black/50 bg-white p-4 w-full justify-center rounded-lg shadow-2xl text-sm dark:bg-gray-800">
-      <ShowTodoTaskButtons></ShowTodoTaskButtons>
-    </div>
-  );
-}
