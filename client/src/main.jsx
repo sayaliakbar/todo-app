a498bb31454514fbcf99b015d1a9e416db7c2af6
Ali Akbar <sayaliakbar@gmail.com>
2025-03-02 03:25:54 +0500

    add: wrap application in StrictMode for improved development practices

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,17 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
+import { StrictMode } from "react";
+
 import App from "./App.jsx";
 import { store } from "./stores/store.js";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  // </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
