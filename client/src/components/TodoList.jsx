423aadd8368114016bfaa9930fdfa34ea06f4bb2
Ali Akbar <sayaliakbar@gmail.com>
2025-03-03 02:38:12 +0500

    add: implement drag-and-drop functionality for todos in TodoList component

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,6 +1,9 @@
 import TodoListFooter from "./TodoListFooter";
 import TodoListTask from "./TodoListTask";
 import { useState } from "react";
+import { DndContext } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
 
 import { useGetTodosQuery } from "../services/todo.js";
 
@@ -15,6 +18,20 @@ export default function TodoList() {
     return true;
   });
 
+  console.log(filteredTodos);
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+    if (active.id !== over.id) {
+      const oldIndex = todos.findIndex((todo) => todo._id === active.id);
+      const newIndex = todos.findIndex((todo) => todo._id === over.id);
+      const newTodos = [...todos];
+      newTodos.splice(oldIndex, 1);
+      newTodos.splice(newIndex, 0, todos[oldIndex]);
+      console.log(newTodos);
+    }
+  };
+
   return (
     <div className="w-full bg-white shadow-2xl rounded-lg overflow-hidden dark:bg-gray-800">
       <ul className="overflow-y-auto max-h-64 w-full  flex flex-col">
@@ -23,14 +40,21 @@ export default function TodoList() {
         ) : isLoading ? (
           <>Loading...</>
         ) : data ? (
-          filteredTodos.map((todo) => (
-            <TodoListTask
-              key={todo._id}
-              id={todo._id}
-              text={todo.title}
-              status={todo.completed}
-            ></TodoListTask>
-          ))
+          <DndContext
+            modifiers={[restrictToVerticalAxis]}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext items={filteredTodos.map((todo) => todo._id)}>
+              {filteredTodos.map((todo) => (
+                <TodoListTask
+                  key={todo._id}
+                  id={todo._id}
+                  text={todo.title}
+                  status={todo.completed}
+                ></TodoListTask>
+              ))}
+            </SortableContext>
+          </DndContext>
         ) : (
           <>No data</>
         )}
