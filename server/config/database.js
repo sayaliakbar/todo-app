a4a8d50df8a414efcc4e55d55f266e93ad62a561
Ali Akbar <sayaliakbar@gmail.com>
2025-03-03 06:01:52 +0500

    fix: improve database connection handling based on NODE_ENV

--- server/config/database.js
+++ server/config/database.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    if (process.env.NODE_ENV === "development") {
+      const conn = await mongoose.connect(process.env.MONGO_URI_DEV);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.bold);
+      console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.bold);
+    } else if (process.env.NODE_ENV === "production") {
+      const conn = await mongoose.connect(process.env.MONGO_URI_PROD);
+
+      console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.bold);
+    } else {
+      console.error(
+        "Invalid NODE_ENV value. Please check your .env file.".red.underline
+          .bold
+      );
+      process.exit(1);
+    }
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
